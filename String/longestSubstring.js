52322bdfb52470c3980807d3368e09138cbe6378
Milon KP <milon.panda333@gmail.com>
2025-10-21 18:44:58 +0530

    string: get longest substring with unique chars

---
+++ String/longestSubstring.js
@@ -0,0 +1,29 @@
+s = "There is Problem in the Solution";
+
+const getLongestUniqueSubstring = function(s) {
+    if ( s.length < 1 ) return "Invalid Input";
+
+    let left = 0;
+    maxLength = 0;
+    const charMap = new Map();
+    let maxSubstring = "";
+
+    for (let right = 0; right < s.length; right++) {
+        const char = s[right];
+        if (charMap.has(char) && charMap.get(char) > left) {
+            left = charMap.get(char) + 1;            
+        }
+
+        charMap.set(char, right);
+        const currentLength = (right - left) + 1;
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
+            maxSubstring = s.substring(left, right);
+        }
+    }
+
+    return maxSubstring;
+}
+
+const answer = getLongestUniqueSubstring(s);
+console.log("answer : " + answer);

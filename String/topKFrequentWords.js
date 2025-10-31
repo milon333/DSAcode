5b58ce9d7966e4e2417f0f53d79298e692c46004
Milon KP <milon.panda333@gmail.com>
2025-10-31 12:59:40 +0530

    Strings: Top K Frequent words

---
+++ String/topKFrequentWords.js
@@ -0,0 +1,25 @@
+const input = ["i","love","leetcode","i","love","coding","coding"], k = 4;
+
+const returnKFrequentWords = function(words, k) {
+
+    const countMap = new Map();
+
+    for (let word of words) {
+        countMap.set(word, (countMap.get(word) + 1) || 1);
+    }
+
+    // sort count map based on their count
+    const sorted = [...countMap.entries()].sort((a, b) => {
+        // sort based on their alphabets
+        if (a[1] === b[1]) return a[0].localeCompare(b[0]);
+
+        // sort descending base on values
+        return b[1] - a[1];
+    });
+
+    // required to return only values - array destructuring
+    return sorted.slice(0, k).map(([word]) => word);
+}
+
+const answer = returnKFrequentWords(input, k);
+console.log("answer :", answer);
